5b9c8acbef2faabbc8031b3ce868c3abd9d1a439
5toHiroki <5toHiroki@gmail.com>
2017-08-27 03:42:42 +0900

    compleate without connection
    Httpとの通信以外完成した。Javaのupdate管理ミスがあった。
    3日目　yunyan(GTO)

--- app/src/main/java/jp/sozolab/agoto/selfflattery/MainActivity.java
+++ app/src/main/java/jp/sozolab/agoto/selfflattery/MainActivity.java
@@ -1,27 +1,98 @@
 package jp.sozolab.agoto.selfflattery;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
 
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
+import java.net.HttpURLConnection;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<String> dataset;
+    private HttpURLConnection connection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView date = (TextView) findViewById(R.id.date);
+        Date now = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM/dd");
+        date.setText(simpleDateFormat.format(now));
+
+        recyclerView = (RecyclerView) findViewById(R.id.list);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // for debug
+        dataset = new ArrayList<String>();
+
+        adapter = new MyAdapter(dataset);
+        recyclerView.setAdapter(adapter);
+
+        TextView score = (TextView) findViewById(R.id.score);
+
+        score.setText("Clear " + Integer.toString(dataset.size()));
     }
 
     /** Called when the user clicks the Send button */
     public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+        dataset.add(message);
+        TextView score = (TextView) findViewById(R.id.score);
+        score.setText("Clear " + Integer.toString(dataset.size()));
+        adapter.notifyDataSetChanged();
+    }
+}
+
+class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private ArrayList<String> dataset;
+
+    public MyAdapter(ArrayList<String> dataset) {
+        this.dataset = dataset;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView textView;
+
+        public ViewHolder(TextView v) {
+            super(v);
+            this.textView = v;
+        }
+
+        public ViewHolder(View v) {
+            super(v);
+            this.textView = (TextView) v;
+        }
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        TextView v = new TextView(parent.getContext());
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.textView.setText(dataset.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataset.size();
     }
 }
