35e79f01446a56bfe6d3d54e5408fc16e01692ef
5toHiroki <5toHiroki@gmail.com>
2017-08-26 18:24:57 +0900

    Update sending message
    アクティビィティ間でメッセージの送受信ができた。
    2日目　Goto

--- app/src/main/java/jp/sozolab/agoto/selfflattery/DisplayMessageActivity.java
+++ app/src/main/java/jp/sozolab/agoto/selfflattery/DisplayMessageActivity.java
@@ -1,7 +1,12 @@
 package jp.sozolab.agoto.selfflattery;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -9,5 +14,14 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
+        layout.addView(textView);
     }
 }
